0c3bae1fce8a3c94504df89c4c000e30a40081ca
davidsodaa <sodakolor@gmail.com>
2017-04-12 17:59:01 +0200

    changes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(5000, function () {
-  console.log('Listening on port 3000!')
+var port = process.env.PORT || 5000
+app.listen(port, function () {
+  console.log('Listening on port ' + port + '!')
 });
